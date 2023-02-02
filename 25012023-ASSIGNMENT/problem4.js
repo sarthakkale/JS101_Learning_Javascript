1183483f98db5587d8fcfd16cccdd70cb555bb3d
sarthakkale <er.sarthakkale@gmail.com>
2023-02-02 16:26:13 +0530

    Introduction_to_JS

---
+++ 25012023-ASSIGNMENT/problem4.js
@@ -0,0 +1,30 @@
+//Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+console.log("");
+console.log("𝕽𝖊𝖕𝖔𝖗𝖙 𝕮𝖆𝖗𝖉");
+
+var name = "Sᴀʀᴛʜᴀᴋ ᴋᴀʟᴇ";
+console.log("𝕹𝖆𝖒𝖊- ",name);
+
+var school = "Gayatri Convent";
+console.log("𝕾𝖈𝖍𝖔𝖔𝖑- ",school);
+
+var grade = "9.8 CGPA";
+console.log("𝕲𝖗𝖆𝖉𝖊- ",grade);
+
+var section = "B";
+console.log("𝕾𝖊𝖈𝖙𝖎𝖔𝖓- ",section);
+
+var rollno = 09;
+console.log("𝕽𝖔𝖑𝖑/𝖓𝖔- ",rollno);
+
+var Physics = 90;
+console.log("𝕻𝖍𝖞𝖘𝖎𝖈𝖘- ",Physics);
+
+var Maths = 85;
+console.log("𝕸𝖆𝖙𝖍𝖘- ",Maths);
+
+var Chemestry = 92;
+console.log("𝕮𝖍𝖊𝖒𝖊𝖘𝖙𝖗𝖞- ",Chemestry);
+
