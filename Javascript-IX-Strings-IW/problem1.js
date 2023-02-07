70d09db5b7a2a2b18d3d20f67ffd886f8afd88d6
sarthakkale <er.sarthakkale@gmail.com>
2023-02-07 17:58:01 +0530

    02/02/2023

---
+++ Javascript-IX-Strings-IW/problem1.js
@@ -0,0 +1,4 @@
+/* Problem 1 : Given a string count the number of words in that string
+*/
+let name = "SARTHAK";
+console.log(name.length);
---
+++ Javascript-IX-Strings-IW/problem2.js
@@ -0,0 +1,6 @@
+/* Problem 2 : Given an array of string count the overall total number of characters
+*/
+let names = ["SARTHAK","TANU","PIYU"];
+for(var i=0;i<names.length;i++){
+  console.log(names[i].length);
+}
